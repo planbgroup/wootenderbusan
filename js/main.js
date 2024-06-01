3359802b7cf4c774924497d16ffa66e4476a5cba
miseullang <155376060+miseullang@users.noreply.github.com>
2024-06-01 16:01:00 +0900

    두번째 섹션 추가

--- js/main.js
+++ js/main.js
@@ -18,4 +18,15 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+
+    // 자동재생
+    autoplay: {
+      delay: 5000,
+    },
+
+    // spaceBetween:50, //=>swiper-slide 사이 공백값
+    slidesPerView:"auto",// =>css에 지정한 슬라이더의 크기를 사용함
+    // =>이값을 지정하지 않으면 기존의 값으로 지정됨
+    centeredSlides:true, //=>슬라이더 번호에 맞는 객체의 가로 중앙 정렬
+
   });
