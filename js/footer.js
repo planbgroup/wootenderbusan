8a8faaf456a2c38bcfa2122b43375b5691b07743
권 윤슬 <yoonseul@planbgroup.org>
2024-06-03 19:54:08 +0900

    푸터 수정

--- js/footer.js
+++ js/footer.js
@@ -6,7 +6,7 @@ class FooterComponent {
       const footerHTML = `
         <footer class="secPadding">
             <a class="botLogo" href="#">
-                <img class="bottomLogo" src="../img/bottomLogo.svg" alt="푸터 로고">
+                <img class="bottomLogo" src="./img/bottomLogo.svg" alt="푸터 로고">
             </a>
             <p class="footText gold">
                 <span>상호명 : 우텐더 해운대</span>
@@ -17,13 +17,13 @@ class FooterComponent {
             <div class="align">
                 <div class="outMenu">
                     <a href="https://map.naver.com/p/entry/place/1175308104?lng=129.1590574&lat=35.1602296&placePath=%2Fbooking%3Fentry%3Dplt&entry=plt&searchType=place&c=15.00,0,0,0,dh" class="reservationIcon iconMenu">
-                        <img src="../img/calendar.svg" alt="예약하기">
+                        <img src="./img/calendar.svg" alt="예약하기">
                     </a>
                     <a href="https://www.instagram.com/wootender_busan" class="instagramIcon iconMenu">
-                        <img src="../img/Instagram.svg" alt="인스타그램">
+                        <img src="./img/Instagram.svg" alt="인스타그램">
                     </a>
                     <a href="https://blog.naver.com/wootenderbusan" class="blogIcon iconMenu">
-                        <img src="../img/blog.svg" alt="블로그">
+                        <img src="./img/blog.svg" alt="블로그">
                     </a>
                 </div>
                 <span class="copyright">Copyright © 2024 The Wootender Haewoondae all right reserved. </span>    
